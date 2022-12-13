da37905199da19a571fa25b852ff64eae8e96c8e
abdelhai95 <abdelhaijamal95@gamil.com>
2022-12-13 23:59:39 +0200

    first version

---
+++ README.md
@@ -0,0 +1,5 @@
+#### React project
+
+-using API
+-building navigation dynamically
+-adding classes dynamically using template strings
---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetching = async () => {
+    const resp = await fetch(url);
+    const newJobs = await resp.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetching();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h2>Looading ...</h2>
+      </section>
+    );
+  }
+
+  const { title, dates, duties, company } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                className={`job-btn ${index === value && "active-btn"}`}
+                onClick={() => setValue(index)}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
+}
+
+export default App;
